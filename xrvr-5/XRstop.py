41decafbe2127c119a2be04387407475cb5d56ae
Giles Heron <giheron@cisco.com>
2016-08-22 19:48:30 +0100

    replaced XRstop.py with XRstop

--- xrvr-5/XRstop.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-processes = os.popen('sudo ps -ef | grep XR')
-for process in processes:
-	words = process.split()
-	if words[7] == 'qemu-system-x86_64':
-		os.popen('sudo kill -9 ' + words[1])
