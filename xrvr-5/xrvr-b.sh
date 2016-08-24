7bcf153bfd26ca026c5295293b61860486d03229
Giles Heron <giheron@cisco.com>
2016-08-24 19:30:38 +0100

    fix port numbers for virtual serial ports

--- xrvr-5/xrvr-b.sh
+++ xrvr-5/xrvr-b.sh
@@ -1,6 +1,6 @@
 sudo qemu-system-x86_64 -daemonize -display none -enable-kvm -machine accel=kvm -smp cores=2 -m 4096 \
   -hda ./xrvr-b.vmdk \
-  -serial telnet::9201,server,nowait -serial telnet::9202,server,nowait \
+  -serial telnet::9021,server,nowait -serial telnet::9022,server,nowait \
   -net nic,model=virtio,vlan=0,macaddr=00:22:00:ff:0B:00 -net tap,vlan=0,script=/etc/qemu-ifup \
   -net nic,model=virtio,vlan=1,macaddr=00:22:00:ff:0B:01 -net socket,vlan=1,connect=127.0.0.1:14000 \
   -net nic,model=virtio,vlan=2,macaddr=00:22:00:ff:0A:02 -net socket,vlan=2,listen=127.0.0.1:17000 \
--- xrvr-5/xrvr-d.sh
+++ xrvr-5/xrvr-d.sh
@@ -1,6 +1,6 @@
 sudo qemu-system-x86_64 -daemonize -display none -enable-kvm -machine accel=kvm -smp cores=2 -m 4096 \
   -hda ./xrvr-d.vmdk \
-  -serial telnet::9401,server,nowait -serial telnet::9402,server,nowait \
+  -serial telnet::9041,server,nowait -serial telnet::9042,server,nowait \
   -net nic,model=virtio,vlan=0,macaddr=00:22:00:ff:0D:00 -net tap,vlan=0,script=/etc/qemu-ifup \
   -net nic,model=virtio,vlan=1,macaddr=00:22:00:ff:0D:01 -net socket,vlan=1,connect=127.0.0.1:16000 \
   -net nic,model=virtio,vlan=2,macaddr=00:22:00:ff:0D:02 -net socket,vlan=2,connect=127.0.0.1:18000 \
