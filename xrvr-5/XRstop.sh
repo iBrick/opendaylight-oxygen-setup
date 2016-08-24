6403ca7bfd5f217e35c0abce5fd147285c397792
Giles Heron <giheron@cisco.com>
2016-08-24 19:34:29 +0100

    fix stop to use node names from file

--- xrvr-5/XRstop.sh
+++ xrvr-5/XRstop.sh
@@ -1,1 +1,9 @@
-sudo kill $(ps aux | grep 'qemu-system-x86_64' | grep 'XR' | awk '{print $2}')
+#!/bin/bash
+
+source ./parameters
+
+while read -r line
+do
+	name=$(echo $line | cut -f2 -d" ")
+	sudo kill -9 $(ps aux | grep 'qemu-system-x86_64' | grep $name | awk '{print $2}')
+done < nodes
