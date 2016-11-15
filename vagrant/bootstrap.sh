b18312174eb50f846934b4d1771bc23811e2bca2
Giles Heron <giheron@cisco.com>
2016-11-15 19:06:45 +0000

    update for Boron-SR1

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -20,7 +20,7 @@ cd ODL
 mkdir images
 cd images
 echo "downloading ODL distro"
-wget -q https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.5.0-Boron/distribution-karaf-0.5.0-Boron.tar.gz
+wget -q https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.5.1-Boron-SR1/distribution-karaf-0.5.1-Boron-SR1.tar.gz
 cd ..
 echo "unpacking/configuring ODL distro"
 ./unpack-odl
